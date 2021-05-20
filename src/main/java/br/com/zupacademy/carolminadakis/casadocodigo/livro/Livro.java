2be6e792c5e51fa9240fd31c29c5b5783ad88e41
Ana Carolina Minadakis <ana.minadakis@zup.com.br>
2021-05-20 17:19:28 -0300

    Cadastro de livro

---
+++ src/main/java/br/com/zupacademy/carolminadakis/casadocodigo/livro/Livro.java
@@ -0,0 +1,97 @@
+package br.com.zupacademy.carolminadakis.casadocodigo.livro;
+
+import br.com.zupacademy.carolminadakis.casadocodigo.autor.modelo.Autor;
+import br.com.zupacademy.carolminadakis.casadocodigo.categoria.modelo.Categoria;
+
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class Livro {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String titulo;
+    @NotBlank
+    @Size(max = 500)
+    private String resumo;
+    @NotBlank
+    private String sumario;
+    @NotNull @Min(20)
+    private BigDecimal preco;
+    @Min(100)
+    private int quantidadePaginas;
+    @NotBlank
+    private String isbn;
+    @Future
+    private LocalDate dataPublicacao;
+    @NotNull
+    @ManyToOne
+    @Valid
+    private Autor autor;
+    @NotNull
+    @ManyToOne
+    @Valid
+    private Categoria categoria;
+
+    @Deprecated
+    public Livro() {
+
+    }
+
+    public Livro(String titulo, String resumo, String sumario,
+                 BigDecimal preco, int quantidadePaginas,
+                 String isbn, LocalDate dataPublicacao,
+                 Autor autor, Categoria categoria) {
+
+        this.titulo = titulo;
+        this.resumo = resumo;
+        this.sumario = sumario;
+        this.preco = preco;
+        this.quantidadePaginas = quantidadePaginas;
+        this.isbn = isbn;
+        this.dataPublicacao = dataPublicacao;
+        this.autor = autor;
+        this.categoria = categoria;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getResumo() {
+        return resumo;
+    }
+
+    public String getSumario() {
+        return sumario;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public int getQuantidadePaginas() {
+        return quantidadePaginas;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public LocalDate getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+}
---
+++ src/main/java/br/com/zupacademy/carolminadakis/casadocodigo/livro/controller/request/LivroRequest.java
@@ -0,0 +1,72 @@
+package br.com.zupacademy.carolminadakis.casadocodigo.livro.controller.request;
+
+import br.com.zupacademy.carolminadakis.casadocodigo.autor.modelo.Autor;
+import br.com.zupacademy.carolminadakis.casadocodigo.categoria.modelo.Categoria;
+import br.com.zupacademy.carolminadakis.casadocodigo.livro.Livro;
+import br.com.zupacademy.carolminadakis.casadocodigo.validarErros.UniqueValue;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.EntityManager;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class LivroRequest {
+
+    @NotBlank
+    @UniqueValue(domainClass = Livro.class, fieldName = "titulo")
+    private String titulo;
+    @NotBlank
+    @Size(max = 500)
+    private String resumo;
+    @NotBlank
+    private String sumario;
+    @NotNull
+    @Min(20)
+    private BigDecimal preco;
+    @Min(100)
+    private int quantidadePaginas;
+
+    @NotBlank
+    @UniqueValue(domainClass = Livro.class, fieldName = "isbn")
+    private String isbn;
+
+    @Future
+    @NotNull
+    @JsonFormat(pattern = "dd/MM/yyyy", shape = JsonFormat.Shape.STRING)
+    private LocalDate dataPublicacao;
+
+    @NotNull
+    @NotNull        //verificar
+    private Long autorId;
+
+    @NotNull
+    @ManyToOne      //verificar
+    private Long categoriaId;
+
+    public LivroRequest(String titulo, String resumo, String sumario,
+                        BigDecimal preco, int quantidadePaginas,
+                        String isbn,
+                        Long autorId, Long categoriaId) {
+        this.titulo = titulo;
+        this.resumo = resumo;
+        this.sumario = sumario;
+        this.preco = preco;
+        this.quantidadePaginas = quantidadePaginas;
+        this.isbn = isbn;
+        this.autorId = autorId;
+        this.categoriaId = categoriaId;
+    }
+
+    public void setDataPublicacao(LocalDate dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    public Livro converte(EntityManager manager) {
+        Autor autor = manager.find(Autor.class, autorId);
+        Categoria categoria =manager.find(Categoria.class, categoriaId);
+        return new Livro(titulo, resumo, sumario, preco, quantidadePaginas,
+                isbn, dataPublicacao, autor, categoria);
+    }
+}
